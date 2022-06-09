bfe746fa6907dde93975f366b0cb2262f52f2d35
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-06-09 21:30:17 +0100

    Created static routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Products = () => <h1>Products</h1>;
+const Contact = () => <h1>Contact</h1>;
 
 const App = () => {
   return (
     <div className="App">
-      React router exercise
+      <Router>
+        <h1>Sample Shop</h1>
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </div>
   );
 }
